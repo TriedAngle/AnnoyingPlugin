2c127186791d1e9cc054b30bd2bf4575b25557da
Sebastian Strobl <Sebastian@Strobl.net>
2019-11-30 21:26:11 +0100

    fixed delay

--- src/main/java/net/strobl/annoyingplugin/events/DeathEvent.java
+++ src/main/java/net/strobl/annoyingplugin/events/DeathEvent.java
@@ -1,10 +1,12 @@
 package net.strobl.annoyingplugin.events;
 
 import net.strobl.annoyingplugin.annoyingplugin.AnnoyingPlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class DeathEvent implements org.bukkit.event.Listener {
 
@@ -17,13 +19,12 @@ public class DeathEvent implements org.bukkit.event.Listener {
   @EventHandler
   public void onDeath(PlayerDeathEvent e){
     Player player = e.getEntity();
-    e.setDeathMessage(ChatColor.YELLOW + "Press " + ChatColor.GOLD + "F " + ChatColor.YELLOW + "to pay respect for " + ChatColor.MAGIC + player.getDisplayName());
-    try {
-      wait(5);
-      main.getServer().broadcastMessage("F");
-    } catch (InterruptedException ex) {
-      ex.printStackTrace();
-    }
+    e.setDeathMessage(ChatColor.YELLOW + "Press " + ChatColor.GOLD + "F " + ChatColor.YELLOW + "to pay respect for " + ChatColor.GOLD + ChatColor.ITALIC + player.getDisplayName());
+    new BukkitRunnable() {
+      @Override
+      public void run(){
+        main.getServer().broadcastMessage("F");
+      }
+    }.runTaskLater(main, 2);
   }
-
 }

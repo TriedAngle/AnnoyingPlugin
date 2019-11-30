1164c31f28b38bdcff8531db2673b30214351b06
Sebastian Strobl <Sebastian@Strobl.net>
2019-11-30 20:07:15 +0100

    added a command which will later list all commands of the plugin

--- src/main/java/net/strobl/annoyingplugin/annoyingplugin/AnnoyingPlugin.java
+++ src/main/java/net/strobl/annoyingplugin/annoyingplugin/AnnoyingPlugin.java
@@ -1,17 +1,36 @@
 package net.strobl.annoyingplugin.annoyingplugin;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AnnoyingPlugin extends JavaPlugin {
 
   @Override
   public void onEnable() {
-    // Plugin startup logic
-
+    System.out.println("Annoyance has been initialized");
   }
 
   @Override
   public void onDisable() {
-    // Plugin shutdown logic
+    System.out.println("This is so sad, Alexa... play Despacito");
   }
+
+
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    if (label.equals("annoy")){
+      if(sender.isOp()){
+        if(sender instanceof Player) {
+          Player player = (Player) sender;
+          player.sendMessage(ChatColor.GOLD + "Annoying Commands:");
+        }
+      } else {
+        sender.sendMessage(ChatColor.RED + "HaHa lol, no permission for that XD");
+      }
+    }
+    return false;
+  }
+
 }

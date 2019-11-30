dbe73a7b7e910d53981735ecb5a6c73c9bff13ea
Sebastian Strobl <Sebastian@Strobl.net>
2019-11-30 19:50:09 +0100

    init project files

---
+++ src/main/java/net/strobl/annoyingplugin/annoyingplugin/AnnoyingPlugin.java
@@ -0,0 +1,17 @@
+package net.strobl.annoyingplugin.annoyingplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnnoyingPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}

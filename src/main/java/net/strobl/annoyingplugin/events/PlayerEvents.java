3bde628120574c0935912dd8398556d15b7d65f3
Sebastian Strobl <Sebastian@Strobl.net>
2019-12-01 13:40:11 +0100

    Death message improvements

--- src/main/java/net/strobl/annoyingplugin/events/PlayerEvents.java
+++ src/main/java/net/strobl/annoyingplugin/events/PlayerEvents.java
@@ -14,35 +14,47 @@ public class PlayerEvents implements org.bukkit.event.Listener {
   private AnnoyingPlugin main;
   private int currently_sleeping;
 
-  public PlayerEvents(AnnoyingPlugin main){
+  public PlayerEvents(AnnoyingPlugin main) {
     this.main = main;
   }
 
   @EventHandler
-  public void onPlayerDeath(PlayerDeathEvent e){
+  public void onPlayerDeath(PlayerDeathEvent e) {
     Player player = e.getEntity();
-    e.setDeathMessage(ChatColor.YELLOW + "Press " + ChatColor.GOLD + "F " + ChatColor.YELLOW + "to pay respect for " + ChatColor.GOLD + ChatColor.ITALIC + player.getDisplayName());
+    main.getServer().broadcastMessage(
+        ChatColor.YELLOW + "Press " + ChatColor.GOLD + "F " + ChatColor.YELLOW
+            + "to pay respect for " + ChatColor.GOLD + ChatColor.ITALIC + player.getDisplayName());
+    main.getServer()
+        .broadcastMessage(ChatColor.YELLOW + "lost: " + ChatColor.GOLD + e.getDroppedExp() + ChatColor.YELLOW + " xp");
+
+    e.setDeathMessage(ChatColor.YELLOW + "cause:" + ChatColor.GOLD + e.getDeathMessage().replace(player.getDisplayName(), ""));
     new BukkitRunnable() {
       @Override
-      public void run(){
-        main.getServer().broadcastMessage("F");
+      public void run() {
+        main.getServer().broadcastMessage(ChatColor.RED + "<Server> " + ChatColor.WHITE + "F");
       }
     }.runTaskLater(main, 2);
   }
 
   @EventHandler
-  public void onPlayerBedEnter(PlayerBedEnterEvent e){
+  public void onPlayerBedEnter(PlayerBedEnterEvent e) {
     Player player = e.getPlayer();
-    currently_sleeping ++;
-    main.getServer().broadcastMessage(ChatColor.GOLD + player.getDisplayName() + ChatColor.YELLOW + " wants to sleep");
-    main.getServer().broadcastMessage(ChatColor.GOLD + String.valueOf(currently_sleeping) + "/" + main.getServer().getOnlinePlayers().size() + ChatColor.YELLOW + " Players are sleeping");
+    currently_sleeping++;
+    main.getServer().broadcastMessage(
+        ChatColor.GOLD + player.getDisplayName() + ChatColor.YELLOW + " wants to sleep");
+    main.getServer().broadcastMessage(
+        ChatColor.GOLD + String.valueOf(currently_sleeping) + "/" + main.getServer()
+            .getOnlinePlayers().size() + ChatColor.YELLOW + " Players are sleeping");
   }
 
   @EventHandler
-  public void onPlayerBedLeave(PlayerBedLeaveEvent e){
+  public void onPlayerBedLeave(PlayerBedLeaveEvent e) {
     Player player = e.getPlayer();
-    currently_sleeping --;
-    main.getServer().broadcastMessage(ChatColor.GOLD + player.getDisplayName() + ChatColor.YELLOW + " left the bed");
-    main.getServer().broadcastMessage(ChatColor.GOLD + String.valueOf(currently_sleeping) + "/" + main.getServer().getOnlinePlayers().size() + ChatColor.YELLOW + " Players are sleeping");
+    currently_sleeping--;
+    main.getServer().broadcastMessage(
+        ChatColor.GOLD + player.getDisplayName() + ChatColor.YELLOW + " left the bed");
+    main.getServer().broadcastMessage(
+        ChatColor.GOLD + String.valueOf(currently_sleeping) + "/" + main.getServer()
+            .getOnlinePlayers().size() + ChatColor.YELLOW + " Players are sleeping");
   }
 }
